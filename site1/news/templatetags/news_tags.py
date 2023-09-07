d855a1a833475bf32698b1c8d2899274aa0ece58
koiv1981 <koiv1981@gmail.com>
2023-09-07 19:13:26 +0300

    NewsSite #3

--- site1/news/templatetags/news_tags.py
+++ site1/news/templatetags/news_tags.py
@@ -1,15 +1,16 @@
 from django import template
 from ..models import Category
+from django.db.models import Count, F
 
 register = template.Library()
 
-@register.simple_tag() # декоратор
+
+@register.simple_tag()
 def get_categories():
     return Category.objects.all()
 
+
 @register.inclusion_tag('news/list_categories.html')
 def show_categories():
-    categories = Category.objects.all()
+    categories = Category.objects.annotate(cnt=Count('news', filter=F('news__is_published'))).filter(cnt__gt=0)
     return {"categories": categories}
-
-
