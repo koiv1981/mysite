add0d4afde62dddb075985b1bd9b0cafef99f77c
koiv1981 <koiv1981@gmail.com>
2023-09-12 18:48:56 +0300

    NewsSite #4 (mixin, pagination)

--- site1/news/views.py
+++ site1/news/views.py
@@ -1,7 +1,9 @@
 # from django.shortcuts import render, get_object_or_404, redirect
 # from django.http import HttpResponse
 from django.views.generic import ListView, DetailView, CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
+
 from .models import News, Category
 from .forms import NewsForm
 
@@ -10,6 +12,7 @@ class HomeNews(ListView):
     model = News
     template_name = 'news/index.html'
     context_object_name = 'news'
+    paginate_by = 5
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super(HomeNews, self).get_context_data(**kwargs)
@@ -25,6 +28,7 @@ class HomeCategory(ListView):
     template_name = 'news/index.html'
     context_object_name = 'news'
     allow_empty = False
+    paginate_by = 5
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super(HomeCategory, self).get_context_data(**kwargs)
@@ -52,7 +56,8 @@ class ViewNews(DetailView):
     context_object_name = 'news_item'
 
 
-class AddNews(CreateView):
+class AddNews(LoginRequiredMixin, CreateView):
     form_class = NewsForm
     template_name = 'news/add_news.html'
     success_url = reverse_lazy('home')
+    login_url = "/admin/"
